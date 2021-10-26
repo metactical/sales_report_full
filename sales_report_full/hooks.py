b2fba3c7d4da33182185c4d6d37b01ad32a6fe60
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-10-26 20:43:36 +0300

    Transfered packing slip customizations to metactical app

--- sales_report_full/hooks.py
+++ sales_report_full/hooks.py
@@ -26,9 +26,9 @@ app_include_css = "/assets/css/sales_report_full.css"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {
-    "Packing Slip" : "public/js/packing_slip.js"
-    }
+#doctype_js = {
+#    "Packing Slip" : "public/js/packing_slip.js"
+#    }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
